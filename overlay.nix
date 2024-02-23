7cd287dfcc9d5e565c613f52af8caa75eaa8bb88
LDprg <lukas_4dr@gmx.at>
2024-02-23 22:02:19 +0100

    fix flakes again

--- overlay.nix
+++ overlay.nix
@@ -14,9 +14,12 @@ final: prev: {
       buildInputs = [ SDL2 spdlog libGL ];
 
       preConfigure = ''
-        cp -r ${bgfx} subprojects/bgfx
-        cp -r ${dylib} subprojects/dylib
         cp -r ${JustEnoughMod} subprojects/JustEnoughMod
+
+        chmod 777 -R subprojects
+
+        cp -r ${bgfx} subprojects/JustEnoughMod/subprojects/bgfx
+        cp -r ${dylib} subprojects/JustEnoughMod/subprojects/dylib
 
         chmod 777 -R subprojects
       '';
@@ -24,9 +27,11 @@ final: prev: {
       installPhase = ''
         mkdir -p $out/bin
         mkdir -p $out/bin/Plugins
+
         cp subprojects/JustEnoughMod/JustEnoughMod $out/bin
         cp subprojects/JustEnoughMod/libJustEnoughMod.so $out/bin
         cp libJustEnoughModCore.so $out/bin/Plugins
+        
         wrapProgram $out/bin/JustEnoughMod \
           --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ libGL ]}
       '';
