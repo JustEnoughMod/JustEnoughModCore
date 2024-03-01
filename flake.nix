f97cd83e21522cebafee9a1e6c0b87591c3f4dc7
LDprg <lukas_4dr@gmx.at>
2024-03-01 17:09:49 +0100

    integrate pre-commit into nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     flake-utils.url = "github:numtide/flake-utils";
     bgfx = {
       url = "https://github.com/LDprg/bgfx.meson";
@@ -22,15 +23,32 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, bgfx, dylib, JustEnoughMod }:
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks, bgfx, dylib, JustEnoughMod }:
     let
       overlay =
         import ./overlay.nix { inherit nixpkgs bgfx dylib JustEnoughMod; };
-    in flake-utils.lib.eachDefaultSystem (system:
+    in
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ overlay ];
         };
-      in { packages.default = pkgs.JustEnoughModCore; });
+      in
+      {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+
+
+              nixpkgs-fmt.enable = true;
+            };
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+        };
+        packages.default = pkgs.JustEnoughModCore;
+      });
 }
